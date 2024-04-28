9a73d00ec1d5b8de41daa1877df80a851e67cfcd
Aarthy <2019e001@eng.jfn.ac.lk>
2024-04-28 21:52:14 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT=5555;
+
+
+export const mongoDBURL=
+'mongodb+srv://Aarthy:F1Adrh1fb7cXRw9k@cluster0.sdb8gqr.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
